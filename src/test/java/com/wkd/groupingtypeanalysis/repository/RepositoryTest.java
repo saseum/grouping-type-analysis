012dd1a076d68548ac60f768bff6aba2f54bb5db
whatkinda <dvlpjh@gmail.com>
2024-02-20 15:55:59 +0900

    test: combination logic test code 추가

--- src/test/java/com/wkd/groupingtypeanalysis/repository/RepositoryTest.java
+++ src/test/java/com/wkd/groupingtypeanalysis/repository/RepositoryTest.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -71,4 +73,57 @@ class RepositoryTest {
         assertThat(allRecords).isNotNull().hasSize(3);
     }
 
+    @DisplayName("Record 전체조회 테스트")
+    @Transactional
+    @Test
+    void findAllRecords() {
+        List<Record> records = recordRepository.findAll();
+        int recordsSize = records.size();
+
+        assertThat(recordsSize).isEqualTo(900);
+    }
+
+    @DisplayName("조합 테스트")
+    @Transactional
+    @Test
+    void combinationConditionallyTest() {
+        List<Record> records = recordRepository.findAll();
+        List<Subject> subjects = subjectRepository.findAll();
+
+        String[] examples = new String[] {"화법과작문", "미적분", "물리학2", "여행지리", "논리학", "논술"};
+
+        int groupingCount = 3;
+
+        List<List<String>> result = combinationSubsets(examples.length, groupingCount, examples);
+
+        result.forEach(subList -> {
+            System.out.print("( ");
+
+            subList.forEach(v -> System.out.print(v + ", "));
+
+            System.out.println(" )");
+        });
+    }
+
+
+    /* combination func (nCm) */
+    private List<List<String>> combinationSubsets(int n, int m, String[] subjects) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(n, m, 0, subjects, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backtrack(int n, int m, int start, String[] subjects, List<String> combination, List<List<String>> result) {
+        if (combination.size() == m) {
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            combination.add(subjects[i]);
+            backtrack(n, m, i + 1, subjects, combination, result);
+            combination.remove(combination.size() - 1);
+        }
+    }
 }
